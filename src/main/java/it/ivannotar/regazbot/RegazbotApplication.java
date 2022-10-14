6e2c1648f02805fc75f8cc2dfa4cd4b77a1140c1
ivan.notarstefano <R1sb33Fr1sb33M!>
2022-10-14 18:07:13 +0200

    Refactoring to Java

---
+++ src/main/java/it/ivannotar/regazbot/RegazbotApplication.java
@@ -0,0 +1,13 @@
+package it.ivannotar.regazbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegazbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegazbotApplication.class, args);
+	}
+
+}
